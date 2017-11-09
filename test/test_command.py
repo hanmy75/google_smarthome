348d8062784a17d498dacac2c3709d15d3c404c3
MY Han <ymhan@Ubuntu14.04.5>
2017-11-09 12:59:35 +0100

    Update test script

--- test/test_command.py
+++ test/test_command.py
@@ -2,12 +2,15 @@ import requests
 # POST with JSON 
 import json
 
+URL = "http://192.168.0.10:4003/smarthome"
 headers = {'authorization': 'Bearer psokmCxKjfhk7qHLeYd1'}
 
+# Example for EXECUTE
 payload = {'commands':[{'devices':[{'id':'2'}],'execution':[{'command':'action.devices.commands.OnOff','params':{'on':'true'}}]}]}
-#contents = {"uid":"1234","auth":"psokmCxKjfhk7qHLeYd1","requestId":"6433088915707706447", 'payload': payload}
-data = {'requestId': 'ff36a3cc-ec34-11e6-b1a0-64510650abcf', 'inputs': [{'intent': 'action.devices.EXECUTE'}, {'payload': payload}]}
+data = {'requestId': 'ff36a3cc-ec34-11e6-b1a0-64510650abcf', 'inputs': [{'intent': 'action.devices.EXECUTE', 'payload': payload}]}
 
-#payload = {'requestId': 'ff36a3cc-ec34-11e6-b1a0-64510650abcf', 'inputs': [{'intent': 'action.devices.SYNC'}]}
+# Example for SYNC
+#data = {'requestId': 'ff36a3cc-ec34-11e6-b1a0-64510650abcf', 'inputs': [{'intent': 'action.devices.SYNC'}]}
 
-requests.post("http://192.168.0.10:4003/smarthome", headers=headers, json=data)
+
+requests.post(URL, headers=headers, json=data)
