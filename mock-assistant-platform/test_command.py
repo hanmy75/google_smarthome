f7a9611fd4b32d3d8b257a70380edf79344e6dd8
MY Han <han.my75@gmail.com>
2017-11-13 16:41:04 +0000

    Update test script

--- mock-assistant-platform/test_command.py
+++ mock-assistant-platform/test_command.py
@@ -2,19 +2,24 @@ import requests
 # POST with JSON
 import json
 
-URL = "http://192.168.0.10:4003/smarthome"
+URL = "http://339a6f04.ngrok.io/smarthome"
 headers = {'authorization': 'Bearer psokmCxKjfhk7qHLeYd1'}
 
 # Example for EXECUTE
-exec_payload = {'commands':[{'devices':[{'id':'2'}],'execution':[{'command':'action.devices.commands.OnOff','params':{'on':'true'}}]}]}
-#data = {'requestId': 'ff36a3cc-ec34-11e6-b1a0-64510650abcf', 'inputs': [{'intent': 'action.devices.EXECUTE', 'payload': exec_payload}]}
+exec_payload = {'commands':[{'devices':[{'id':'1'}],'execution':[{'command':'action.devices.commands.OnOff','params':{'on':'true'}}]}]}
+exec_data = {'requestId': 'ff36a3cc-ec34-11e6-b1a0-64510650abcf', 'inputs': [{'intent': 'action.devices.EXECUTE', 'payload': exec_payload}]}
 
 # Example for SYNC
-#data = {'requestId': 'ff36a3cc-ec34-11e6-b1a0-64510650abcf', 'inputs': [{'intent': 'action.devices.SYNC'}]}
+sync_data = {'requestId': 'ff36a3cc-ec34-11e6-b1a0-64510650abcf', 'inputs': [{'intent': 'action.devices.SYNC'}]}
 
 # Example for QUERY
-query_payload = {'devices': {'1': {'on': 'true', 'online': 'true'}, '2': {'on': 'true', 'online': 'true'}, '3': {'on': 'true', 'online': 'true'}, '4': {'on': 'true', 'online': 'true'}}}
-data = {'requestId': 'ff36a3cc-ec34-11e6-b1a0-64510650abcf', 'inputs': [{'intent': 'action.devices.QUERY', 'payload': query_payload}]}
+query_payload = {'devices': [{'id': '1'}, {'id': '2'}, {'id': '3'}, {'id':'4'}]}
+query_data = {'requestId': 'ff36a3cc-ec34-11e6-b1a0-64510650abcf', 'inputs': [{'intent': 'action.devices.QUERY', 'payload': query_payload}]}
 
 
-requests.post(URL, headers=headers, json=data)
+requests.post(URL, headers=headers, json=sync_data)
+
+requests.post(URL, headers=headers, json=exec_data)
+
+requests.post(URL, headers=headers, json=query_data)
+
